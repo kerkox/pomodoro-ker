e08cb7ad912686baa0a3bf7b0a475f562f4e73ba
Paul Cortes <paulkerg@gmail.com>
2020-05-16 16:28:37 -0500

    Agregado navbar y uso de bootstrap

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Menu.css'
 
 const Menu = () => {
   return (
-    <nav className="">
-      <Link className="" to="/">
-        Home
-      </Link>
-      <Link className="" to="/settings">
-        Settings
-      </Link>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <span className="navbar-brand">
+        <div class="clock icon"></div>        
+      </span>      
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/settings">
+              Settings
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
