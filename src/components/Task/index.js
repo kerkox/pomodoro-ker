63201bfe97a29af3bb9d8d99f97a71d6609bc23b
Paul Cortes <paulkerg@gmail.com>
2020-05-17 22:27:48 -0500

    [Add] component Task

---
+++ src/components/Task/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Task extends Component {
+  render() {
+    return (
+      <div>
+        Task
+      </div>
+    )
+  }
+}
+
+export default Task
